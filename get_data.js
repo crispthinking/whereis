39b106ed367f9e263e4fa81479eb3cde91d94fb1
Carlos Aragon <carlos.aragon@crispthinking.com>
2023-11-09 13:59:36 +0100

    change

--- get_data.js
+++ get_data.js
@@ -1,18 +1,20 @@
 
 async function getWhoIsData(url) {
     fetch('https://who.is/whois/' + url, {
-   headers: {
-      'Accept': 'application/json'
-   }
-}).then(function(response) {return response.text();})
+        method: "GET", mode: 'cors',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(function (response) { return response.text(); })
 }
 
 async function getGeoIpData(ipAdress, token) {
-    fetch('https://geoip.maxmind.com/geoip/v2.1/city/'+ ipAdress + '?demo=1', {
-   headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Bearer ' + token
-   }
-}).then(function(response) {return response.json();})
+    fetch('https://geoip.maxmind.com/geoip/v2.1/city/' + ipAdress + '?demo=1', {
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    }).then(function (response) { return response.json(); })
 }
 
+const xpath = '//div[@class="row queryResponseBodyRow"]//div[@class="col-md-8 queryResponseBodyValue"]//a'
